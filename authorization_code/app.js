5ff6b86462f4167b769485b7d0afacf630f8a14a
markjnkim <mark.jn.kim@gmail.com>
2019-02-15 10:36:24 -0800

    added express for static files, changed relative paths in index/<head> for css and index.html/<js>

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = 'd54087e878404de2a91f33a710bd690b'; // Your client id
+var client_secret = 'e5236688dfda4fc3a509c7b4e5ca2c61'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
